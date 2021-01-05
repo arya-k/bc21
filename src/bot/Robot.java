764615bd6ed2d80df91236dd6fa2de1bfbcc56d3
Charlie Gunn <cjgunnmail@gmail.com>
2021-01-05 15:54:18 -0500

    merge

--- src/bot/Robot.java
+++ src/bot/Robot.java
@@ -8,10 +8,18 @@ import static bot.Communication.decode;
 abstract class Robot {
     static RobotController rc = null;
     static RobotType type = null;
+    static int centerID;
+    static Direction[] directions = Direction.values();
 
     public static void init(RobotController rc) throws GameActionException {
         Robot.rc = rc;
-        type = rc.getType();
+        Robot.type = rc.getType();
+        // find the EC
+        for (RobotInfo info : rc.senseNearbyRobots(2)) {
+            if (info.getType() == RobotType.ENLIGHTENMENT_CENTER && info.getTeam() == rc.getTeam()) {
+                Robot.centerID = info.getID();
+            }
+        }
     }
 
     abstract void onAwake() throws GameActionException;
