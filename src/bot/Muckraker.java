ce7eff89e7030981b62e6c5696f876ccf0ddd541
Steven Qu <stevenqu2001@gmail.com>
2021-01-05 15:27:17 -0500

    muckracker exposes slanderer on sight

--- src/bot/Muckraker.java
+++ src/bot/Muckraker.java
@@ -1,8 +1,7 @@
 package bot;
 
-import battlecode.common.Direction;
-import battlecode.common.GameActionException;
-import battlecode.common.MapLocation;
+import battlecode.common.*;
+
 
 public class Muckraker extends Robot {
 
@@ -11,17 +10,34 @@ public class Muckraker extends Robot {
     @Override
     void onAwake() {
         Nav.init(Muckraker.rc); // Initialize the nav
-        goalPos = rc.getLocation().translate(3, 3);
+        goalPos = rc.getLocation().translate(100, 0);
         Nav.setGoal(goalPos);
     }
 
     @Override
     void onUpdate() throws GameActionException {
-        Nav.tick();
-
         // early stopping
         if (rc.getLocation().distanceSquaredTo(goalPos) == 0) {
-            rc.resign();
+//            rc.resign();
         }
+
+        // check if action possible
+        if (rc.isReady()) {
+            // get all enemy nearby robots
+            RobotInfo[] neighbors = rc.senseNearbyRobots(12, rc.getTeam().opponent());
+            for (RobotInfo neighbor : neighbors) {
+                // expose first slanderer found
+                if (neighbor.getType() == RobotType.SLANDERER) {
+                    rc.expose(neighbor.getLocation());
+                    Clock.yield();
+                    return;
+                }
+            }
+            // otherwise move
+            Nav.tick();
+        }
+
+        // end turn
+        Clock.yield();
     }
 }
